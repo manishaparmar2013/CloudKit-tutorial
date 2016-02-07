0b02fece07f600fdd8625b96b29ea818582db4bb
manishaparmar2013 <manishaparmar@manishas-macbook-air.local>
2016-02-07 18:27:26 +1100

    Initial Commit

---
+++ testcloudkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testcloudkit
+//
+//  Created by Manisha Parmar on 7/02/2016.
+//  Copyright © 2016 Manisha Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
